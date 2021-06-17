b5a2795554ac805f3313988b516b053ae6a7d6f4
sunnyvenkat789 <74831879+sunnyvenkat789@users.noreply.github.com>
2021-06-17 18:26:31 +0530

    Update hippo-request-submission.php

--- hippoform/hippo-request-submission.php
+++ hippoform/hippo-request-submission.php
@@ -3,50 +3,51 @@
 require( '../../../wp-load.php' );
 
 	global $wpdb;
-
 if (($_POST['hippo_email']!="")){
 	$hippo_email = $_POST['hippo_email'];
 	$first_name = $_POST['first_name'];
 	$middle_name = $_POST['middle_name'];
 	$last_name = $_POST['last_name'];
 	$street = $_POST['street'];
+	$street = str_replace(' ', '%20', $street);
 	$unit = $_POST['unit'];
 	$city = $_POST['city'];
+	$city = str_replace(' ', '%20', $city);
 	$state = $_POST['state'];
 	$zip_code = $_POST['zip_code'];
 	$date_of_birth = $_POST['date_of_birth'];
-	$date_of_birth1 = date('Y-m-d',strtotime($date_of_birth));
+	$date_of_birth1 = date('dmY',strtotime($date_of_birth));
 	$phone = $_POST['phone'];
 	$house_type = $_POST['house_type'];
 	$curl = curl_init();
  $api_url = get_option("api_url");
  $auth_code = get_option("auth_code");
- curl_setopt_array($curl, array(
-   CURLOPT_URL => $api_url,
-   CURLOPT_RETURNTRANSFER => true,
-   CURLOPT_ENCODING => "",
-   CURLOPT_MAXREDIRS => 10,
-   CURLOPT_TIMEOUT => 30,
-   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-   CURLOPT_CUSTOMREQUEST => "POST",
-   CURLOPT_POSTFIELDS => "{ \n  id:\"\",\n  street: \"$street\",\n  city: \"$city\",\n  state: \"$state\",\n  zip: \"$zip_code\",\n  first_name: \"$first_name\",\n  last_name: \"$last_name\",\n  email: \"$hippo_email\",\n  phone: \"$phone\",\n  date_of_birth: \"$date_of_birth\" }",
-   CURLOPT_HTTPHEADER => array(
-     'auth_token: ' . $auth_code,
-	 "cache-control: no-cache",
-	 "content-type: application/json",
-   ),
- ));
+	$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $api_url.'?auth_token='.$auth_code.'&street='.$street.'&city='.$city.'&zip='.$zip_code.'&first_name='.$first_name.'&last_name='.$last_name.'&email='.$hippo_email.'&phone='.$phone.'&state='.$state.'&date_of_birth='.$date_of_birth1,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+	$result = json_decode($response, true);
+	echo 'Quote Premium: ' . $result['quote_premium'] . '<br />';
  
- $response = curl_exec($curl);
- $err = curl_error($curl);
+}
  
- curl_close($curl);
- 
- if ($err) {
-   echo "cURL Error #:" . $err;
- } else {
-   echo $response; 
- }
   $tablename = $wpdb->prefix."hippo_request";
 	$wpdb->insert(
 		$tablename,
